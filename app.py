12b56d1d3a01e15fdfb7ad7ceffed84ac0d0a7b9
cristianeaguiar <cristiane.aguiarsa@gmail.com>
2024-09-25 21:25:52 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,15 @@
+# importa a biblioteca
+import time
+
+print("Abrindo programa...")
+time.sleep(3)
+print("Execultando programa...")
+time.sleep(3)
+# importa a biblioteca
+import time
+
+print("Abrindo programa...")
+time.sleep(3)
+print("Execultando programa...")
+time.sleep(3)
+print("Programa execultado com sucesso!")
